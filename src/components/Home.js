ac8ce52c8c01867e483db817a783e38f89fe4b77
Bo Latt <bolattny@gmail.com>
2022-11-16 04:01:00 -0500

    move iframe player to Video component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import "./Home.css";
-import YouTube from "react-youtube";
+
+const popularVideos = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${process.env.REACT_APP_API_KEY}`;
 
 const Home = () => {
   const [apidata, setApidata] = useState([]);
-
-  const popularVideos = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${process.env.REACT_APP_API_KEY}`;
 
   useEffect(() => {
     fetch(`${popularVideos}`)
@@ -13,30 +12,12 @@ const Home = () => {
       .then((data) => setApidata(data.items));
   }, []);
 
-  const opts = {
-    height: "390",
-    width: "640",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-      enablejsapi: 1,
-    },
-  };
-
-  function onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  }
-
   console.log(apidata);
 
   return (
     <div className="home">
       {apidata.map(({ id, snippet }) => {
         return (
-          // <div key={video.id}>
-          //   <YouTube videoId={video.id} opts={opts} onReady={onReady} />
-          // </div>
           <div className="card">
             <img src={snippet.thumbnails.high.url} />
             <h4>{snippet.title}</h4>
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,7 +1,28 @@
 import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
+
 import "./Video.css";
+
+const opts = {
+  height: "390",
+  width: "640",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 0,
+    enablejsapi: 1,
+  },
+};
+
+function onReady(event) {
+  // access to player in all event handlers via event.target
+  event.target.pauseVideo();
+}
 
 export default function Video() {
   const { id } = useParams;
-  return <div>Video</div>;
+  return (
+    <div>
+      <YouTube videoId={id} opts={opts} onReady={onReady} />
+    </div>
+  );
 }
