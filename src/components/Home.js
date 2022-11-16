cf13744779ee4ee457d99b987b8b2fc287068c47
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-16 00:04:09 -0500

    nav bar complete, home and about components created

---
+++ src/components/Home.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "./Home.css";
+import YouTube from "react-youtube";
+
+const Home = () => {
+  const [apidata, setApidata] = useState([]);
+
+  const popularVideos = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${process.env.REACT_APP_API_KEY}`;
+
+  useEffect(() => {
+    fetch(`${popularVideos}`)
+      .then((res) => res.json())
+      .then((data) => setApidata(data.items));
+  }, []);
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  function onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  console.log(apidata);
+
+  return (
+    <div>
+      {apidata.map((video) => {
+        return (
+          <div key={video.id}>
+            <YouTube videoId={video.id} opts={opts} onReady={onReady} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
