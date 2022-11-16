b799aa9bb220abe86c389c85c9d1cb4d67c91947
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-15 21:02:46 -0500

    initial commit, fetch and trello board set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [apidata, setApidata] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then(res => res.json())
+    .then(setApidata)
+  }, [])
+
+  console.log(apidata)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
