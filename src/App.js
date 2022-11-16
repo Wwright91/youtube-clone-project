ab2eee6fff3c0ecea5f002623a4c53bdf14cd732
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-15 21:40:45 -0500

    fetch made to popular videos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [apidata, setApidata] = useState([]);
 
-  const [apidata, setApidata] = useState([])
+  const popularVideos = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${process.env.REACT_APP_API_KEY}`
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+      `${popularVideos}`
     )
-      .then(res => res.json())
-    .then(setApidata)
-  }, [])
+      .then((res) => res.json())
+      .then(setApidata);
+  }, []);
 
-  console.log(apidata)
+  console.log(apidata);
 
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
