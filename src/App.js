92428fe91088a1a7ca3da1a03c14993a277bf8c3
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-15 22:01:33 -0500

    fetching popular videos via youtube-react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import "./App.css";
 
 function App() {
@@ -11,12 +12,33 @@ function App() {
       `${popularVideos}`
     )
       .then((res) => res.json())
-      .then(setApidata);
+      .then(data => setApidata(data.items));
   }, []);
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  function onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
 
   console.log(apidata);
 
-  return <div className="App"></div>;
+  return <div className="App">
+    {apidata.map(video => {
+      return( <div key={video.id}>
+        <YouTube videoId={  video.id} opts={opts} onReady={onReady}/>
+      </div>)
+    })}
+  
+  </div>;
 }
 
 export default App;
