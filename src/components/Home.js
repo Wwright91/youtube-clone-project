ca0014fd24c5097a0626805027a54022de456254
Bo Latt <bolattny@gmail.com>
2022-11-16 22:06:43 -0500

    add duration, channel title and views to video card details

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,14 +15,48 @@ const Home = () => {
 
   console.log(apidata);
 
+  function formatDuration(duration) {
+    duration = duration.slice(2);
+    duration = duration.replace("H", ":");
+    duration = duration.replace("M", ":");
+    duration = duration.replace("S", "");
+    return duration;
+  }
+
+  function formatViewsCount(views) {
+    if (views.length >= 4 && views.length < 7) {
+      views = views.slice(0, -3) + "," + views.slice(-3);
+    } else if (views.length >= 7) {
+      if (views[1] !== "0") {
+        views = views[0] + "." + views[1] + "M";
+      } else {
+        views = views[0] + "M";
+      }
+    }
+    return views;
+  }
+
   return (
     <div className="home">
-      {apidata.map(({ id, snippet }) => {
+      {apidata.map(({ id, snippet, statistics, contentDetails }) => {
         return (
           <Link key={id} to={`videos/${id}`}>
             <div className="card">
-              <img src={snippet.thumbnails.high.url} />
+              <div class="img-wrapper">
+                <img src={snippet.thumbnails.high.url} />
+                <span id="duration">
+                  {formatDuration(contentDetails.duration)}
+                </span>
+              </div>
+
               <h4>{snippet.title}</h4>
+              <div className="details">
+                <p>{snippet.channelTitle}</p>
+                <p>
+                  {/* <span> {formatDuration(contentDetails.duration)} </span> */}
+                  <span> {formatViewsCount(statistics.viewCount)} Views</span>
+                </p>
+              </div>
             </div>
           </Link>
         );
