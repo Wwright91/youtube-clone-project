5d67c11cd458f9f6cbf1656d7ccb4da16b98ff2d
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-17 10:34:58 -0500

    search feature basic functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,12 @@ const popularVideos = `https://youtube.googleapis.com/youtube/v3/videos?part=sni
 
 const Home = () => {
   const [apidata, setApidata] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [searchedData, setSearchedData] = useState([]);
+  const [submit, setSubmit] = useState(false);
+
+  //searchvideos
+  //  'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=[YOUR_API_KEY]'
 
   useEffect(() => {
     fetch(`${popularVideos}`)
@@ -14,19 +20,71 @@ const Home = () => {
   }, []);
 
   console.log(apidata);
+  console.log(searchedData);
+
+  // function handleInput(e) {
+  //   let input = e.target.value
+  //   setInputValue(input)
+  // }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!inputValue) {
+      alert("Please input a value");
+    } else {
+      fetch(
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${inputValue}&key=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setSearchedData(data.items);
+        });
+      setInputValue("");
+      setSubmit(true);
+    }
+  }
 
   return (
-    <div className="home">
-      {apidata.map(({ id, snippet }) => {
-        return (
-          <Link key={id} to={`videos/${id}`}>
-            <div className="card">
-              <img src={snippet.thumbnails.high.url} />
-              <h4>{snippet.title}</h4>
-            </div>
-          </Link>
-        );
-      })}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search Videos:
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+
+      <div className="home">
+        {submit
+          ? searchedData.map(({ id, snippet }) => {
+              return (
+                <Link key={id} to={`videos/${id.videoId}`}>
+                  <div className="card">
+                    <img src={snippet.thumbnails.high.url} />
+                    <h4>{snippet.title}</h4>
+                  </div>
+                </Link>
+              );
+            })
+          : apidata.map(({ id, snippet }) => {
+              return (
+                <Link key={id} to={`videos/${id}`}>
+                  <div className="card">
+                    <img src={snippet.thumbnails.high.url} />
+                    <h4>{snippet.title}</h4>
+                  </div>
+                </Link>
+              );
+            })}
+      </div>
     </div>
   );
 };
