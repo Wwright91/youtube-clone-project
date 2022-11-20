2654531d533a5b67bbab88e9e5fb4f62397e81b9
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-20 16:24:58 -0500

    video description added

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import YouTube from "react-youtube";
 import Comments from "./Comments";
+import { formatViewsCount } from "./HelperFunctions";
 
 import "./Video.css";
 
@@ -22,9 +24,30 @@ function onReady(event) {
 export default function Video() {
   const { id } = useParams();
   console.log(id);
+  const [videoDetails, setVideoDetails] = useState([])
+
+  useEffect(() => {
+  fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${process.env.REACT_APP_API_KEY}`)
+  .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.items)
+    setVideoDetails(data.items)
+  });
+  }, []);
+  
+  console.log(videoDetails)
+
   return (
     <div className="videopage">
       <YouTube videoId={id} opts={opts} />
+      {videoDetails.map(({ snippet, statistics }) => {
+        return (<div>
+          <h2>{snippet.title}</h2>
+          <p>{snippet.description}</p>
+          <p>Uploaded on {snippet.publishedAt}</p>
+          <p>{formatViewsCount(statistics.viewCount)} views</p>
+          </div>)
+      })}
       <Comments id={id} />
     </div>
   );
