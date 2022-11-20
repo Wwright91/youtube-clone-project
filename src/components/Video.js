08faf8d55c7369d54b017993ec2c8bf50cade210
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-20 03:28:43 -0500

    comments section added

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import YouTube from "react-youtube";
+import Comments from "./Comments";
 
 import "./Video.css";
 
@@ -22,8 +23,9 @@ export default function Video() {
   const { id } = useParams();
   console.log(id);
   return (
-    <div>
+    <div className="videopage">
       <YouTube videoId={id} opts={opts} />
+      <Comments id={id} />
     </div>
   );
 }
