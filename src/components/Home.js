ad8bcf74f245748f040f81c289ba6276adbc33ed
Bo Latt <bolattny@gmail.com>
2022-11-21 20:48:39 -0500

    infinite scroll workign on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import "./Home.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import VideoList from "./VideoList";
 import Modal from "./ErrorModal";
+import "./Home.css";
 
 const popularVideosUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=21&regionCode=US&key=${process.env.REACT_APP_API_KEY}`;
 
@@ -9,15 +11,17 @@ const Home = () => {
   const [popularVideos, setPopularVideos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [loadingError, setLoadingError] = useState(false);
+  const [pageToken, setPageToken] = useState(null);
 
   useEffect(() => {
     fetch(`${popularVideosUrl}`)
       .then((res) => {
-        if (!res.ok)
-          throw new Error("Throwing Throwing")
-       return res.json()
+        if (!res.ok) throw new Error("Throwing Throwing");
+        return res.json();
       })
       .then((data) => {
+        console.log(data.nextPageToken);
+        setPageToken(data.nextPageToken);
         setPopularVideos(data.items);
         setLoading(false);
         setLoadingError(false);
@@ -29,12 +33,53 @@ const Home = () => {
       });
   }, []);
 
+  const fetchData = () => {
+    console.log(`${popularVideosUrl}&pageToken=${pageToken}`);
+    fetch(`${popularVideosUrl}&pageToken=${pageToken}`)
+      .then((res) => {
+        if (!res.ok) throw new Error("Throwing Throwing");
+        return res.json();
+      })
+      .then((data) => {
+        setPageToken(data.nextPageToken);
+        setPopularVideos([...popularVideos, ...data.items]);
+        setLoading(false);
+        setLoadingError(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoadingError(true);
+      });
+  };
+  console.log(popularVideos);
   return (
-    <div>
-      {loadingError ? <Modal loadingError={loadingError} setLoadingError={setLoadingError}/> :
-        <VideoList videos={popularVideos} kind="popular" loading={loading} loadingError={loadingError} />
-      }
-    </div>
+    <main>
+      <InfiniteScroll
+        dataLength={popularVideos.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {loadingError ? (
+          <Modal
+            loadingError={loadingError}
+            setLoadingError={setLoadingError}
+          />
+        ) : (
+          <VideoList
+            videos={popularVideos}
+            kind="popular"
+            loading={loading}
+            loadingError={loadingError}
+          />
+        )}
+      </InfiniteScroll>
+    </main>
   );
 };
 
